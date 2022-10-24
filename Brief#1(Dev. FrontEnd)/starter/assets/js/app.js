dec1f8817333373aeee10c91d7410631c5b558ba
abdelkrim-jouidri <abdelkrimjouidri@gmail.com>
2022-10-24 09:02:23 +0100

    added all functionalities

--- Brief#1(Dev. FrontEnd)/starter/assets/js/app.js
+++ Brief#1(Dev. FrontEnd)/starter/assets/js/app.js
@@ -4,14 +4,7 @@
  * 
  */
 
-//  const task =  {
-//     title : titleInput.value,
-//     type : flexRadioDefault1.value || flexRadioDefault2.value,
-//     priority : priorityOptions.value,      
-//     Status: statusOptions.value,        
-//     date : taskDate.value,          
-//     description : taskDescription.value  ,
-//         }
+// Declaration of DOM objects
 const toDoContainer = document.getElementById('to-do-tasks')
 const inProgressContainer = document.getElementById('in-progress-tasks')
 const DoneContainer = document.getElementById('done-tasks')
@@ -20,103 +13,12 @@ const doneHeadingContainer = document.getElementById('doneHeadingContainer')
 const toDoHeadingContainer = document.getElementById('toDoHeadingContainer')
 const featureOption = document.getElementById('featureOption')
 const bugFeature = document.getElementById('bugOption')
-
-
-
 const saveBtn = document.getElementById('saveChangesButton')
 const titleInput = document.getElementById('titleInput')
 const priorityOptions = document.getElementById('priorityOptions')
 const statusOptions = document.getElementById('statusOptions')
 const taskDate = document.getElementById('taskDate')
 const taskDescription = document.getElementById('taskDescription')
-
-// deleteBtn.addEventListener('click', ()=>console.log('clicked'))
-// console.log(deleteBtn)
-function setId(){
-    for(let task of tasks) task.id = tasks.findIndex(el => el===task)
-}
-setId()
-console.log(tasks)
-
-function doneMarkUp(task){
-    return `<button class="border-0 border-top py-2 d-flex gap-4 btnParent" data-id="${task.id}" >
-    <div class="">
-        <i class="bi bi-check2-circle fs-15px text-success"></i> 
-    </div>
-    <div class="text-start w-100">
-        <div class="fw-bold">${task.title}</div>
-        <div class="">
-            <div class="">#${task.id+1} created in ${task.date}</div>
-            <div class="" title="as they can be helpful in reproducing the steps that caused the problem in the first place.">${task.description}</div>
-        </div>
-        <div class="d-flex justify-content-between">
-        <div class="">
-            <span class="btn btn-primary btn-xs">${task.priority}</span>
-            <span class="btn btn-secondary btn-xs">${task.type}</span>
-        </div>
-        <div class="">
-            <span class="btn btn-danger btn-xs deleteBtn" onClick = "deleteTask()" >Delete</span>
-            <span class="btn btn-info btn-xs">Edit</span>
-        </div>
-    </div>
-    </div>
-</button>`
-}
-
-function inProgressMarkUp(task){
-    return `<button class="border-0 border-top py-2 d-flex gap-4 btnParent" data-id="${task.id}">
-    <div class="spinner-border text-success spinner-border-sm" role="status">
-        <span class="visually-hidden">Loading...</span>
-    </div>
-    <div class="text-start w-100">
-        <div class="fw-bold">${task.title}</div>
-        <div class="">
-            <div class="">#${task.id+1} created in ${task.date}</div>
-            <div class="" title="including as many details as possible.">including as many details as possible.</div>
-        </div>
-    <div class="d-flex justify-content-between">
-        <div class="">
-            <span class="btn btn-primary btn-xs">${task.priority}</span>
-            <span class="btn btn-secondary btn-xs">${task.type}</span>
-        </div>
-        <div class="">
-            <span class="btn btn-danger btn-xs deleteBtn" onClick = "deleteTask()" >Delete</span>
-            <span class="btn btn-info btn-xs">Edit</span>
-        </div>
-    </div>
-    </div>
-</button>`
-}
-
-function toDoMarkUp(task){
-    return `<button class="border-0 border-top py-2 d-flex gap-4 btnParent" data-id="${task.id}">
-    <div class="">
-        <i class="bi bi-question-circle-fill fs-15px text-success"></i> 
-    </div>
-    <div class="text-start w-100">
-        <div class="fw-bold">${task.title}</div>
-        <div class="">
-            <div class="">#${task.id+1} created in ${task.date}</div>
-            <div class="" title="as they can be helpful in reproducing the steps that caused the problem in the first place.">${task.description}</div>
-        </div>
-        <div class="d-flex justify-content-between">
-        <div class="">
-            <span class="btn btn-primary btn-xs">${task.priority}</span>
-            <span class="btn btn-secondary btn-xs">${task.type}</span>
-        </div>
-        <div class="">
-            <span class="btn btn-danger btn-xs deleteBtn" onClick = "deleteTask()">Delete</span>
-            <span class="btn btn-info btn-xs">Edit</span>
-        </div>
-    </div>
-    </div>
-</button>`
-}
-
-showTasks()
-console.log( bugFeature.value)
-saveBtn.addEventListener('click', saveTask)
-
 
 function showTasks(){
     toDoContainer.innerHTML = '';
@@ -130,27 +32,92 @@ function showTasks(){
                                         ${tasks.filter(el=> el.status==='To Do').length}</span>)</h4>`
     tasks.forEach(task => {
 
-        if(task.status === 'To Do') toDoContainer.insertAdjacentHTML('beforeend', toDoMarkUp(task))
-        if(task.status === 'In Progress') inProgressContainer.insertAdjacentHTML('beforeend', inProgressMarkUp(task))
-        if(task.status === 'Done') DoneContainer.insertAdjacentHTML('beforeend', toDoMarkUp(task))
+        if(task.status === 'To Do') toDoContainer.insertAdjacentHTML('beforeend', createButtonTemplate(task))
+        if(task.status === 'In Progress') inProgressContainer.insertAdjacentHTML('beforeend', createButtonTemplate(task))
+        if(task.status === 'Done') DoneContainer.insertAdjacentHTML('beforeend', createButtonTemplate(task))
     })
+    //clear submitted data from input fields
+    titleInput.value = ''
+    priorityOptions.value = 'none'
+    statusOptions.value='none'
+    taskDate.value = "dd-mm-yyyy"
+    taskDescription.value=''
    
 }
 
+function setId(){
+    for(let task of tasks) task.id = tasks.findIndex(el => el===task)
+}
 
+// Show all the tasks from the 'tasks' array once the page is fully loaded
+window.onload = ()=> {
+    showTasks()
+    setId()
+}
 
-function createTask() {
-    // initialiser task form
-    // Afficher le boutton save
-
-    // Ouvrir modal form
+function createButtonTemplate(task){
+    let icon = (task.status=='To Do') ? `<div class=""><i class="bi bi-question-circle-fill fs-15px text-success"></i> </div>` : (task.status=='In Progress') ?
+    `<div class="spinner-border text-success spinner-border-sm" role="status"> <span class="visually-hidden">Loading...</span> </div>` :
+    `<div class=""><i class="bi bi-check2-circle fs-15px text-success"></i></div>`;
     
+    return `<button class="border-0 border-top py-2 d-flex gap-4 btnParent" data-id="${task.id}" >
+        ${icon}
+    <div class="text-start w-100">
+        <div class="fw-bold">${task.title}</div>
+        <div class="">
+            <div class="">#${task.id+1} created in ${task.date}</div>
+            <div class="" title="as they can be helpful in reproducing the steps that caused the problem in the first place.">${task.description}</div>
+        </div>
+        <div class="d-flex justify-content-between">
+        <div class="">
+            <span class="btn btn-primary btn-xs">${task.priority}</span>
+            <span class="btn btn-secondary btn-xs">${task.type}</span>
+        </div>
+        <div class="">
+            <span class="btn btn-danger btn-xs deleteBtn" onClick = "deleteTask()" >Delete</span>
+            <span class="btn btn-info btn-xs editBtn" type="button" data-bs-toggle="modal" data-bs-target="#editTaskModal"  onclick="handleEdit()"">Edit</span>
+        </div>
+    </div>
+    </div>
+</button>`
+
+}
+
+function handleEdit(){
+    // Get the the id of the selected task button
+    let index = window.event.target.closest('.btnParent').dataset.id
+
+    // Retrieve the data of the input fields  from the selected task button
+    document.getElementById('editTitleInput').value = tasks[index].title
+    document.getElementById('editTaskDescription').value = tasks[index].description
+    tasks[index].type ==='Feature' ? document.getElementById('edit-featureOption').checked=true : document.getElementById('edit-bugOption').checked=true
+    document.getElementById('editTaskDate').value = tasks[index].date
+    document.getElementById('editStatusOptions').value = tasks[index].status
+    document.getElementById('editTaskPriorityOptions').value = tasks[index].priority
+
+    //listen for the click event on the save button from the edit modal and call the edit and show function
+    document.getElementById('saveEditsButton').onclick = ()=>{
+        editAndShow(index)
+        document.getElementById('closeEditsButton').click()
+    };
+}
+
+
+function editAndShow(index){
+    // overwrite the data of the task object of the selected button with the altered data from the input fields 
+    tasks[index].title = document.getElementById('editTitleInput').value 
+    tasks[index].description = document.getElementById('editTaskDescription').value 
+    tasks[index].priority = document.getElementById('editTaskPriorityOptions').value 
+    tasks[index].date = document.getElementById('editTaskDate').value 
+    tasks[index].status = document.getElementById('editStatusOptions').value
+    //update the state of all the tasks buttons
+    showTasks()
 }
 
 function saveTask() {
     // Recuperer task attributes a partir les champs input
+    // Créez task object
 
-    
     const task =  {
             title : titleInput.value ,
             type : featureOption.checked? featureOption.value : bugFeature.value,
@@ -160,51 +127,26 @@ function saveTask() {
             description : taskDescription.value ,
             id : tasks.length
                 }
-
-    // Créez task object
-    
+    if(!titleInput.value || !priorityOptions.value || !statusOptions.value || !taskDescription.value ) {
+        alert('empty input fields')
+        return ;
+    }
     // Ajoutez object au Array
     
     tasks.push(task)
     setId()
-    console.log(tasks)
+
     // refresh tasks
+
     document.getElementById('closeModalButton').click()
     showTasks()
 
 }
 
-function editTask(index) {
-    // Initialisez task form
-
-    // Affichez updates
-
-    // Delete Button
-
-    // Définir l’index en entrée cachée pour l’utiliser en Update et Delete
-
-    // Definir FORM INPUTS
-
-    // Ouvrir Modal form
-}
-
-function updateTask() {
-    // GET TASK ATTRIBUTES FROM INPUTS
-
-    // Créez task object
-
-    // Remplacer ancienne task par nouvelle task
-
-    // Fermer Modal form
-
-    // Refresh tasks
-    
-}
 
 function deleteTask() {
     // Get index of task in the array
-    let clickTarget = window.event.target
-    const index = clickTarget.closest('.btnParent').dataset.id
+    const index = window.event.target.closest('.btnParent').dataset.id
     // Remove task from array by index splice function
     tasks.splice(index,1)
     setId()
@@ -212,16 +154,4 @@ function deleteTask() {
     console.log(index)
     // refresh tasks
     showTasks()
-}
-
-function initTaskForm() {
-    // Clear task form from data
-
-    // Hide all action buttons
-}
-
-function reloadTasks() {
-    // Remove tasks elements
-
-    // Set Task count
 }
